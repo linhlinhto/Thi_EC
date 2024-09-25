8491e9b7a8289b85db7661a376ca7039d1f5eaa0
linhlinhto <118755644+linhlinhto@users.noreply.github.com>
2024-09-25 22:42:17 +0700

    Add files via upload

--- Source_code/NV1.ino
+++ Source_code/NV1.ino
@@ -1,1 +1,20 @@
+#include <Leanbot.h>
 
+void setup() {
+  Leanbot.begin();                  // initialize Leanbot
+}
+
+void loop() {
+  LbMotion.runLR(+400, +400);       // let Leanbot go straight
+  LbMotion.waitDistance(4000);      // wait for Leanbot to reach 4,000 steps
+  LbMotion.stopAndWait();           // let Leanbot stop slowly
+
+  LbDelay(3000);                    // delay 3s
+
+
+  LbMotion.runLR(-400, -400);       // let Leanbot go backwards
+  LbMotion.waitDistance(4000);      // wait for Leanbot back 4,000 steps
+  LbMotion.stopAndWait();
+
+  LbDelay(3000);
+}
