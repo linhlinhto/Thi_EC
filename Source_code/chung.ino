a58eae70b4daa1771440545a240968170c9e4ba8
linhlinhto <118755644+linhlinhto@users.noreply.github.com>
2024-09-27 13:26:09 +0700

    Update chung.ino

--- Source_code/chung.ino
+++ Source_code/chung.ino
@@ -38,7 +38,94 @@ void loop() {
 }
 
 void Mission1(){
+  while(!LbIRLine.isBlackDetected()){
+    LbMotion.runLR(mspeedL, mspeedR);
+  }
+  unsigned int d = Leanbot.pingCm();
+  while( LbIRLine.isBlackDetected()) {
+    if( d >= 5  ){
+      runFollowLine();
+      LbMotion.runLR(mspeedL, mspeedR);
+      d = Leanbot.pingCm();
+    }
+    else if(d < 5){
+      
+      LbMotion.runLR(0,0);
+      int eyeleft = LbIRArray.read(ir6L);
+      int eyeright = LbIRArray.read(ir7R);
+      if(eyeleft < eyeright){
+        LbMotion.runRotationDeg(-mspeedL, mspeedR, -45);
+        while(!LbIRLine.isBlackDetected()){
+        LbMotion.runLR(mspeedL, mspeedR/3);
+      }
+      }
+      else{
+        LbMotion.runRotationDeg(mspeedL, -mspeedR, 45);
+        while(!LbIRLine.isBlackDetected()){
+        LbMotion.runLR(mspeedL/3, mspeedR);
+        }
+      }
+    }
+  }
+  LbMotion.runLR(mspeedL, mspeedR);
+  LbMotion.waitDistanceMm(43);
+  LbGripper.close();
+  LbMotion.runLR(mspeedL, mspeedR);
+  LbMotion.waitDistanceMm(43);
+  LbMotion.runRotationDeg(-mspeedL, mspeedR, -90);
+  mission1sucess = true;
 
+}
+
+void runFollowLine() {
+  byte lineValue = LbIRLine.read();      // Read the value of 4 line sensors
+  LbIRLine.displayOnRGB(CRGB::DarkCyan); // Display the result on RGB Leds for observation
+ 
+  switch (lineValue) {                   // check the position of the black line relative to Leanbot
+    case 0b0100:                         // .o.. - if the black line off to the left
+      mspeedL = 1500;
+      mspeedR = 2000;                     
+      break;
+    case 0b1110:                         // ooo.
+      mspeedL = 1000;
+      mspeedR = 2000;                     //        turn left
+      break;
+
+    case 0b1100:                         // oo..
+      mspeedL = 1000;
+      mspeedR = 2000; 
+      break; 
+    case 0b1000:                         // o...
+      mspeedL = -2000;
+      mspeedR = 2000;  
+      break;
+
+    case 0b0010:                         // ..o. - if the black line off to the right
+      mspeedL = 2000;
+      mspeedR = 1500; 
+      break;
+    case 0b0111:                         // .ooo
+      mspeedL = 2000;
+      mspeedR = 1000;         //        turn right
+      break;
+
+    case 0b0011:                         // ..oo
+      mspeedL = 2000;
+      mspeedR = 1000; 
+      break;
+    case 0b0001:                         // ...o
+      mspeedL = 2000;
+      mspeedR = -2000;        //        spin right
+      break;
+    case 0b0110:                         // ...o
+      mspeedL = 2000;
+      mspeedR = 2000;        //        spin right
+      break; 
+      
+    default:                             // all other cases
+      mspeedL = 2000;
+      mspeedR = 2000;   //        go straight
+  }
 }
 
 void Mission2(){
@@ -51,7 +138,7 @@ void Mission3(){
 
 void Mission4(){
   LbMotion.runLR(mspeedL, mspeedR);
-  int distanceleft = LbIRArray.read(ir6L);        // doc gia tri cam bien
+  int distanceleft = LbIRArray.read(ir6L);        // doc gia tri cacm bien
   int distanceright = LbIRArray.read(ir7R);
   while(distanceleft >= 760 && distanceright >= 760){   // cho den khi vao duong di
     distanceleft = LbIRArray.read(ir6L);      
@@ -61,12 +148,12 @@ void Mission4(){
     distanceleft = LbIRArray.read(ir6L);
     distanceright = LbIRArray.read(ir7R);     // kiem tra lai gia tri
 
-    if(distanceleft< distanceright-5){       // neu ben trai gan hon
+    if(distanceleft< distanceright-10){       // neu ben trai gan hon
       mspeedR = 0;                        
       mspeedL = 2000;             //giam toc do banh phai
     }
 
-    else if(distanceright< distanceleft-5){     // neu ben phai gan hon
+    else if(distanceright< distanceleft-10){     // neu ben phai gan hon
       mspeedR = 2000;
       mspeedL = 0;            // giam toc do banh trai
     }
@@ -79,7 +166,7 @@ void Mission4(){
     LbMotion.runLR(mspeedL, mspeedR);  //  dat toc do cho 2 banh 
   }
 
-  LbMotion.waitDistanceMm(20);   //  sau khi het duong di them 2 cm de den giua o dich
+  LbMotion.waitDistanceMm(20);   //  sau khi het duong phai ne di them 2 cm de den giua o dich
   LbMotion.runLR(0, 0);  //  dung xe
   mission4sucess = true;  
 }
