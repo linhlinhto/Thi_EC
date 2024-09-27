6c6c1c1d8d57a2bc72643db082f54358652265a2
linhlinhto <118755644+linhlinhto@users.noreply.github.com>
2024-09-28 01:06:23 +0700

    Update NV1.ino

--- Source_code/NV1.ino
+++ Source_code/NV1.ino
@@ -1,34 +1,35 @@
 void Mission1(){
+  unsigned int obstacledist = 200;
   while(!LbIRLine.isBlackDetected()){
     LbMotion.runLR(mspeedL, mspeedR);
   }
   unsigned int d = Leanbot.pingCm();
   while( LbIRLine.isBlackDetected()) {
     if( d >= 5  ){
-      runFollowLine();
-      LbMotion.runLR(mspeedL, mspeedR);
+      followLine();
       d = Leanbot.pingCm();
-      LbDelay(50);
     }
     else if(d < 5){
       
       LbMotion.runLR(0,0);
-      int eyeleft = LbIRArray.read(ir6L);
-      int eyeright = LbIRArray.read(ir7R);
+      int eyeleft = irScan(ir6L, obstacledist);
+      int eyeright = irScan(ir7R, obstacledist);
       if(eyeleft < eyeright){
-        LbMotion.runRotationDeg(-mspeedL, mspeedR, -45);
+        LbMotion.runRotationDeg(-SPEED_MAX/2, SPEED_MAX/2, -45);
         while(!LbIRLine.isBlackDetected()){
-        LbMotion.runLR(mspeedL, mspeedR/3);
+        LbMotion.runLR(SPEED_MAX, SPEED_MAX/3);
       }
       }
       else{
-        LbMotion.runRotationDeg(mspeedL, -mspeedR, 45);
+        LbMotion.runRotationDeg(SPEED_MAX/2, -SPEED_MAX/2, 45);
         while(!LbIRLine.isBlackDetected()){
-        LbMotion.runLR(mspeedL/3, mspeedR);
+        LbMotion.runLR(SPEED_MAX/3, SPEED_MAX);
         }
       }
     }
   }
+  mspeedL = 2000;
+  mspeedR = 2000;
   LbMotion.runLR(mspeedL, mspeedR);
   LbMotion.waitDistanceMm(43);
   LbGripper.close();
@@ -37,55 +38,4 @@ void Mission1(){
   LbMotion.runRotationDeg(-mspeedL, mspeedR, -90);
   mission1sucess = true;
 
-}
-
-void runFollowLine() {
-  byte lineValue = LbIRLine.read();      // Read the value of 4 line sensors
-  LbIRLine.displayOnRGB(CRGB::DarkCyan); // Display the result on RGB Leds for observation
- 
-  switch (lineValue) {                   // check the position of the black line relative to Leanbot
-    case 0b0100:                         // .o.. - if the black line off to the left
-      mspeedL = 1500;
-      mspeedR = 2000;                     
-      break;
-    case 0b1110:                         // ooo.
-      mspeedL = 1000;
-      mspeedR = 2000;                     //        turn left
-      break;
-
-    case 0b1100:                         // oo..
-      mspeedL = 1000;
-      mspeedR = 2000; 
-      break; 
-    case 0b1000:                         // o...
-      mspeedL = -2000;
-      mspeedR = 2000;  
-      break;
-
-    case 0b0010:                         // ..o. - if the black line off to the right
-      mspeedL = 2000;
-      mspeedR = 1500; 
-      break;
-    case 0b0111:                         // .ooo
-      mspeedL = 2000;
-      mspeedR = 1000;         //        turn right
-      break;
-
-    case 0b0011:                         // ..oo
-      mspeedL = 2000;
-      mspeedR = 1000; 
-      break;
-    case 0b0001:                         // ...o
-      mspeedL = 2000;
-      mspeedR = -2000;        //        spin right
-      break;
-    case 0b0110:                         // ...o
-      mspeedL = 2000;
-      mspeedR = 2000;        //        spin right
-      break; 
-      
-    default:                             // all other cases
-      mspeedL = 2000;
-      mspeedR = 2000;   //        go straight
-  }
 }
